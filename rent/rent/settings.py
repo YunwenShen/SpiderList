4aac3acccdbb7ed126d5a576c385141a71eb2176
yunwen <shenyunwen520@qq.com>
2017-12-23 23:31:51 +0800

    初始化项目

--- rent/rent/settings.py
+++ rent/rent/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'rent.spiders'
 #USER_AGENT = 'rent (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- rent/rent/spiders/woAiWoJia.py
+++ rent/rent/spiders/woAiWoJia.py
@@ -2,7 +2,7 @@
 import scrapy
 
 
-class WoaiwojiaSpider(scrapy.Spider):
+class WoAiWoJiaSpider(scrapy.Spider):
     name = 'woAiWoJia'
     allowed_domains = ['https://hz.5i5j.com/zufang']
     start_urls = ['http://https://hz.5i5j.com/zufang/']
